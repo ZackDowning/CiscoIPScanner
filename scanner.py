2df8bdd6fbe394235f31b3a42f6661e407440b06
ZackDowning <zackarytdowning@gmail.com>
2021-07-17 19:36:04 -0500

    AW0.4

--- scanner.py
+++ scanner.py
@@ -2,15 +2,13 @@ from ipaddress import IPv4Network, AddressValueError
 from general import mt
 import exceptions as e
 from address_validator import ipv4
+from progressbar import progressbar
 # import time
-
-# TODO: Add check to see if interface already exists on ios and nxos for source vlan
-# TODO: Check to make sure vlan exists in vlan db
-# TODO: break down Scan class
-# TODO: Add check to make sure provided intf ip address is within subnet to scan for
 
 
 def hosts_lists_parse(prefix, all_hosts):
+    """Splits host list into seperate lists for concurrent SSH or TELNET sessions for faster IP scan\n
+    if prefix length is between 29 and 23"""
     if prefix == 29:
         return [
             all_hosts[0:2], all_hosts[2:4], all_hosts[4:6]
@@ -32,18 +30,18 @@ def hosts_lists_parse(prefix, all_hosts):
         ]
     elif prefix == 25:
         return [
-            all_hosts[0:18], all_hosts[18:36], all_hosts[19:54], all_hosts[54:72],
+            all_hosts[0:18], all_hosts[18:36], all_hosts[36:54], all_hosts[54:72],
             all_hosts[72:90], all_hosts[89:108], all_hosts[108:126]
         ]
     elif prefix == 24:
         return [
             all_hosts[0:25], all_hosts[25:50], all_hosts[50:75], all_hosts[75:100],
-            all_hosts[100:104], all_hosts[125:149], all_hosts[150:174], all_hosts[175:199],
-            all_hosts[200:224], all_hosts[225:249], all_hosts[250:253]
+            all_hosts[100:125], all_hosts[125:150], all_hosts[150:175], all_hosts[175:200],
+            all_hosts[200:225], all_hosts[225:250], all_hosts[250:254]
         ]
     elif prefix == 23:
         return [
-            all_hosts[0:51], all_hosts[51:102], all_hosts[102:153], all_hosts[103:204],
+            all_hosts[0:51], all_hosts[51:102], all_hosts[102:153], all_hosts[153:204],
             all_hosts[204:255], all_hosts[255:306], all_hosts[306:357], all_hosts[357:408],
             all_hosts[408:459], all_hosts[459:510]
         ]
@@ -51,26 +49,27 @@ def hosts_lists_parse(prefix, all_hosts):
         raise e.SubnetTooLarge
 
 
-class Scan:
-    def __init__(self, network, devicetype, source_vlan, connection, create_intf=False,
-                 intf_ip=None, vrf=None, count=2, timeout=1):
-        self.network = network
-        self.devicetype = devicetype
-        self.reachable_devices = []
-        self.devices = []
-        self.all_hosts = []
-        session = connection.connection().session
+class ProgressBar:
+    def __init__(self, iterable, prefix):
+        self.bar = progressbar(iterable, prefix=prefix)
 
-        # Input error checking
+
+class Scan:
+    """Initiates IP scan of IP subnet from Cisco IOS or NX-OS devices\n
+    Subnet can be between a /29 and /23"""
+    def __init__(self, network, devicetype, source_vlan, connection, create_intf=False, intf_ip=None, vrf=None,
+                 count=2, timeout=1, progress_bar=False):
+
         # Intf IP required if creating interface
         if create_intf and intf_ip is None:
             raise e.NoIntfIPSpecified
 
-        # Intf IP required if NXOS
+        # Intf IP required if NX-OS
         if devicetype == 'cisco_nxos' and intf_ip is None:
             raise e.NoNXOSIntfIPSpecified
 
-        if int(source_vlan - 1) not in range(4091):
+        # Checks VLAN to make sure valid VLAN ID within extended VLAN range and not within reserved VLAN ID range
+        if int(source_vlan) not in range(1, 4095) or int(source_vlan) in range(1002, 1006):
             raise e.InvalidVlanID
 
         # Checks to make sure network is valid
@@ -84,11 +83,13 @@ class Scan:
         if intf_ip is not None:
             if not ipv4(intf_ip):
                 raise e.InvalidInterfaceIP
-            if any(intf_ip != h1 for h1 in hosts_r):
+            if not any(intf_ip == h1 for h1 in hosts_r):
                 raise e.InterfaceIPAddressNotInNetwork
-
+        
+        # Checks for source vlan in device VLAN database
+        session = connection.connection().session
         vlan_db = session.send_command('show vlan brief', use_textfsm=True)
-        if any(source_vlan != v1['vlan_id'] for v1 in vlan_db):
+        if not any(source_vlan == v1['vlan_id'] for v1 in vlan_db):
             raise e.VlanNotInVlanDB
 
         # Creates Interface
@@ -97,8 +98,7 @@ class Scan:
                 session.send_config_set([
                     f'interface vlan {source_vlan}',
                     f'ip address {intf_ip} {network.netmask}',
-                    'no shut',
-                    'do wr'
+                    'no shut'
                 ])
             else:
                 if devicetype == 'cisco_nxos':
@@ -109,10 +109,12 @@ class Scan:
                     f'interface vlan {source_vlan}',
                     vrf_cmd,
                     f'ip address {intf_ip} {network.netmask}',
-                    'no shut',
-                    'do wr'
+                    'no shut'
                 ])
-
+                
+        # Creating list of dictionaries for hosts for later sorted of unordered output data from scan
+        self.all_hosts = []
+        """All hosts in specified subnet with dictionaries including reachability info, ip address, and mac address"""
         for h in hosts_r:
             self.all_hosts.append(
                 {
@@ -120,15 +122,19 @@ class Scan:
                 }
             )
 
+        reachable_devices = []
+
         def scan(host, conn):
+            """Main IP subnet Scan function"""
             ip_address = host['address']
-            # TODO: Add interface format checking
-            if self.devicetype == 'cisco_ios':
+            # Formats input command for proper formatting based on if IOS(-XE) or NX-OS operating system
+            # Also checks to make sure device type is IOS(-XE) or NX-OS
+            if devicetype == 'cisco_ios':
                 if vrf is None:
                     cmd = f'ping {ip_address} repeat {count} timeout {timeout} source vlan {source_vlan}'
                 else:
                     cmd = f'ping vrf {vrf} {ip_address} repeat {count} timeout {timeout} source vlan {source_vlan}'
-            elif self.devicetype == 'cisco_nxos':
+            elif devicetype == 'cisco_nxos':
                 if vrf is None:
                     cmd = f'ping {ip_address} count {count} timeout {timeout} source {intf_ip}'
                 else:
@@ -137,9 +143,10 @@ class Scan:
                 raise e.InvalidDeviceType
 
             cmd_output = conn.send_command(cmd)
-
+            
+            # Checks for cmd input errors
             if cmd_output.__contains__('Invalid'):
-                if self.devicetype == 'cisco_ios':
+                if devicetype == 'cisco_ios':
                     if cmd_output.__contains__(
                             'Invalid source interface - Interface vrf does not match the vrf used for ping'):
                         raise e.NoVRFSpecifiedWithIntInVRF
@@ -155,57 +162,49 @@ class Scan:
                     if cmd_output.__contains__('Invalid host/interface'):
                         raise e.InvalidIntf
             else:
-                if self.devicetype == 'cisco_ios':
+                # Checks if device recieved ping echo then appending IP address to list if non-0 value
+                if devicetype == 'cisco_ios':
                     try:
                         if str(cmd_output.split('\n')[4].split(' ')[3]) != '0':
-                            self.reachable_devices.append(ip_address)
+                            reachable_devices.append(ip_address)
                     except IndexError:
                         if str(cmd_output.split('\n')[5].split(' ')[3]) != '0':
-                            self.reachable_devices.append(ip_address)
+                            reachable_devices.append(ip_address)
                 else:
                     if str(cmd_output.split('\n')[5].split(' ')[3]) != '0':
-                        self.reachable_devices.append(ip_address)
+                        reachable_devices.append(ip_address)
 
+        # Splits hosts list into mulitple smaller lists for for multiple asyncronous SSH/TELNET sessions
         hosts_lists = hosts_lists_parse(int(network.prefixlen), self.all_hosts)
+
+        self.list_num = 1
 
         def host_split(host_list):
             session1 = connection.connection().session
-            for h1 in host_list:
-                scan(h1, session1)
+            if progress_bar:
+                bar = ProgressBar(host_list, f'Phase {str(self.list_num)}: ').bar
+                self.list_num += 1
+                for h1 in bar:
+                    scan(h1, session1)
+            else:
+                for h1 in host_list:
+                    scan(h1, session1)
             session1.disconnect()
-
-        # start = time.perf_counter()
+        
         mt(host_split, hosts_lists, threads=len(hosts_lists))
-        # end = time.perf_counter()
-        # print(f'finished in {round(end - start, 0)} seconds')
 
         arps = session.send_command(f'show ip arp vlan {source_vlan}', use_textfsm=True)
 
         # Creates Interface
         if create_intf:
-            session.send_config_set([f'no interface vlan {source_vlan}', 'do wr'])
-
+            session.send_config_set([f'no interface vlan {source_vlan}'])
         session.disconnect()
 
-        def append(arp):
-            if arp['mac'].count('.') == 2:
-                self.devices.append(
-                    {
-                        'address': arp['address'],
-                        'mac': arp['mac']
-                    }
-                )
-
-        mt(append, arps)
-
         def sort(host):
-            for device in self.devices:
-                if device['address'] == host['address']:
-                    host['mac'] = device['mac']
-            if any(
-                    reachable_device for reachable_device in self.reachable_devices
-                    if host['address'] == reachable_device
-            ):
+            for arp in arps:
+                if arp['address'] == host['address'] and arp['mac'].count('.') == 2:
+                    host['mac'] = arp['mac']
+            if any(host['address'] == reachable_device for reachable_device in reachable_devices):
                 host['status'] = 'Reachable'
             else:
                 host['status'] = 'Unreachable'
--- testing.py
+++ testing.py
@@ -1,32 +1,39 @@
 import os
 from scanner import Scan
 from general import Connection
-from progressbar import progressbar
+# from progressbar import progressbar
 # from pprint import pp
 
 username = os.getenv('USERNAME')
 password = os.getenv('PASSWORD')
-mgmt_ip = '10.187.30.2'
-devicetype = 'cisco_nxos'
+mgmt_ip = '10.3.0.1'
+devicetype = 'cisco_ios'
 
+# networks = [
+#     '10.39.18.0/24', '10.39.20.0/24', '10.39.21.0/24', '10.39.22.0/24', '10.39.23.0/24', '10.39.1.0/24',
+#     '10.39.4.0/24', '192.168.253.0/24'
+# ]
+#
+# vlans = ['18', '20', '21', '22', '23', '192', '253', '2001', '2004']
 networks = [
-    '10.39.18.0/24', '10.39.20.0/24', '10.39.21.0/24', '10.39.22.0/24', '10.39.23.0/24', '10.39.1.0/24',
-    '10.39.4.0/24', '192.168.253.0/24'
+    '10.3.0.0/24'
 ]
 
-vlans = ['18', '20', '21', '22', '23', '192', '253', '2001', '2004']
+vlans = ['10']
 
 with open('netscan.csv', 'w+') as file:
     length = len(networks) * len(vlans)
     file.write('ip_address,mac_address,status,vlan\n')
-    vlan_bar = progressbar(vlans, prefix='VLANS: ')
-    for vlan in vlan_bar:
-        network_bar = progressbar(networks, prefix='Networks: ')
-        for network in network_bar:
-            n = network.split('.')
-            intf_ip = f'{n[0]}.{n[1]}.{n[2]}.3'
+    # vlan_bar = progressbar(vlans, prefix='VLANS: ')
+    # for vlan in vlan_bar:
+    for vlan in vlans:
+        # network_bar = progressbar(networks, prefix='Networks: ')
+        # for network in network_bar:
+        for network in networks:
+            # n = network.split('.')
+            # intf_ip = f'{n[0]}.{n[1]}.{n[2]}.3'
             connection = Connection(mgmt_ip, username, password, devicetype)
-            all_hosts = Scan(network, devicetype, vlan, connection, True, intf_ip).all_hosts
+            all_hosts = Scan(network, devicetype, vlan, connection, progress_bar=True).all_hosts
             for host in all_hosts:
                 address = host['address']
                 try:
